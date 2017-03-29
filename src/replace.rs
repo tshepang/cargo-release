ce30bd017efb5d09c42cb8b07cb9fd6d7526e356
Ning Sun <sunng@about.me>
2017-03-29 16:58:09 +0800

    (feat) better error handling
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/replace.rs
+++ src/replace.rs
@@ -28,15 +28,21 @@ pub fn do_replace_versions(replace_config: &Value,
     if let &Value::Array(ref v) = replace_config {
         for tbl in v {
             if let &Value::Table(ref t) = tbl {
-                let file = t.get("file").and_then(|v| v.as_str()).unwrap();
-                let pattern = t.get("search").and_then(|v| v.as_str()).unwrap();
-                let replace = t.get("replace").and_then(|v| v.as_str()).unwrap();
+                let file = try!(t.get("file")
+                                    .and_then(|v| v.as_str())
+                                    .ok_or(FatalError::ReplacerConfigError));
+                let pattern = try!(t.get("search")
+                                       .and_then(|v| v.as_str())
+                                       .ok_or(FatalError::ReplacerConfigError));
+                let replace = try!(t.get("replace")
+                                       .and_then(|v| v.as_str())
+                                       .ok_or(FatalError::ReplacerConfigError));
                 let replace_with_version = replace.replace("{{version}}", version);
                 let replacer = replace_with_version.as_str();
 
                 let data = try!(load_from_file(&Path::new(file)));
 
-                let r = Regex::new(pattern).unwrap();
+                let r = try!(Regex::new(pattern).map_err(FatalError::from));
                 let result = r.replace_all(&data, replacer);
 
                 if !dry_run {
