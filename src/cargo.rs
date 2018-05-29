5402efab0dcf9c47b00388e67ad4cd107db9afd6
Ning Sun <sunng@protonmail.com>
2018-05-29 20:38:33 +0800

    (feat) use current cargo

--- src/cargo.rs
+++ src/cargo.rs
@@ -2,13 +2,13 @@ use cmd::call;
 use error::FatalError;
 
 pub fn publish(dry_run: bool) -> Result<bool, FatalError> {
-    call(vec!["cargo", "publish"], dry_run)
+    call(vec![env!("CARGO"), "publish"], dry_run)
 }
 
 pub fn update(dry_run: bool) -> Result<bool, FatalError> {
-    call(vec!["cargo", "update"], dry_run)
+    call(vec![env!("CARGO"), "update"], dry_run)
 }
 
 pub fn doc(dry_run: bool) -> Result<bool, FatalError> {
-    call(vec!["cargo", "doc", "--no-deps"], dry_run)
+    call(vec![env!("CARGO"), "doc", "--no-deps"], dry_run)
 }
