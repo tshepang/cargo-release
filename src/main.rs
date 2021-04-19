0367df744b8ee05d5d317aa2e0bbfbf7f5ce8bcb
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-04-19 10:47:13 +0300

    Log message to inform about publish grace sleeping.

--- src/main.rs
+++ src/main.rs
@@ -710,6 +710,7 @@ fn release_packages<'m>(
                 // HACK: Even once the index is updated, there seems to be another step before the publish is fully ready.
                 // We don't have a way yet to check for that, so waiting for now in hopes everything is ready
                 if !dry_run {
+                    log::info!("Waiting an additional {} seconds for crates.io to update its indices...", args.config.publish_grace_sleep);
                     std::thread::sleep(std::time::Duration::from_secs(args.config.publish_grace_sleep));
                 }
             } else {
